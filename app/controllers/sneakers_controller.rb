78ed0f89c3575225f177a8299231a184ad71f362
Sara Levi <saralevi524@gmail.com>
2021-11-15 17:09:52 +0100

    create

--- app/controllers/sneakers_controller.rb
+++ app/controllers/sneakers_controller.rb
@@ -10,4 +10,20 @@ class SneakersController < ApplicationController
   def new
     @sneaker = Sneaker.new
   end
+
+  def create
+    @sneaker = Sneaker.new(sneaker_params)
+    # we need `restaurant_id` to associate review with corresponding restaurant
+    if @sneaker.save
+      redirect_to sneaker_path(@Sneaker)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def sneaker_params
+    params.require(:sneaker).permit(:brand, :name, :size, :price)
+  end
 end
