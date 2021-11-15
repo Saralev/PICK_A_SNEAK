17992d32a0e8e639882e0e2b2421ea958a62c6b6
Albin Le Bon <lebon.albin@gmail.com>
2021-11-15 16:26:02 +0100

    adding booking migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_151621) do
+ActiveRecord::Schema.define(version: 2021_11_15_151959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.boolean "approved"
+    t.bigint "user_id", null: false
+    t.bigint "sneaker_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["sneaker_id"], name: "index_bookings_on_sneaker_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "sneakers", force: :cascade do |t|
     t.string "brand"
@@ -41,5 +53,7 @@ ActiveRecord::Schema.define(version: 2021_11_15_151621) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "sneakers"
+  add_foreign_key "bookings", "users"
   add_foreign_key "sneakers", "users"
 end
