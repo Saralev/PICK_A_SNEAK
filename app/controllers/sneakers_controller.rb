a4b0fafa6295cc14a5c4f9abfdd8bf489021e627
Sara Levi <saralevi524@gmail.com>
2021-11-15 16:55:45 +0100

    new

--- app/controllers/sneakers_controller.rb
+++ app/controllers/sneakers_controller.rb
@@ -2,4 +2,12 @@ class SneakersController < ApplicationController
   def index
     @sneakers = Sneakers.all
   end
+
+  def show
+    @sneaker = Sneaker.find(params[:id])
+  end
+
+  def new
+    @sneaker = Sneaker.new
+  end
 end
