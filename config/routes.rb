90e43d6088dbedc919b57b38ac1feb3cf424cafc
Albin Le Bon <lebon.albin@gmail.com>
2021-11-16 12:47:36 +0100

    adding dashboard page + controller + view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/dashboard', to: 'pages#dashboard', as: 'dashboard'
 
   resources :sneakers, only: [:index, :show, :new, :create] do
     resources :bookings, only: [:new, :create]
-   end
   end
+end
