32ed34e4b8eb8a8ad68140a64ec01400d4dc6b52
Albin Le Bon <lebon.albin@gmail.com>
2021-11-16 14:51:41 +0100

    fixing 500

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :sneakers
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :sneakers
 end
