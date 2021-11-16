33a903ec05fb5f1f2daf27204bffa400347d22fa
Albin Le Bon <lebon.albin@gmail.com>
2021-11-16 18:13:12 +0100

    adding image to show and index

--- app/models/sneaker.rb
+++ app/models/sneaker.rb
@@ -1,9 +1,10 @@
 class Sneaker < ApplicationRecord
+  has_one_attached :image
+
   belongs_to :user
   validates :brand, presence: true
   validates :name, presence: true
   validates :size, presence: true
   validates :price, presence: true
 
-  has_one_attached :image
 end
