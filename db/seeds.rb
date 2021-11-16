96fbb419447f7c57ade4d46411241f8437e6fa8a
Albin Le Bon <lebon.albin@gmail.com>
2021-11-16 14:06:12 +0100

    adding puts and destroy DB before creating

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts "Cleaning up database..."
+User.destroy_all
+Sneaker.destroy_all
+Booking.destroy_all
+puts "Database cleaned"
+
+puts "Creating users..."
 elsa = User.create(first_name: "Elsa", last_name:"Test", email: "elsa@gmail.com", password:"123456", password_confirmation:"123456")
 benjamin = User.create(first_name: "Benjamin", last_name:"Test", email: "benjamin@gmail.com", password:"123456", password_confirmation:"123456")
 chloé = User.create(first_name: "Chloé", last_name:"Test", email: "chloé@gmail.com", password:"123456", password_confirmation:"123456")
 eric = User.create(first_name: "Eric", last_name:"Test", email: "eric@gmail.com", password:"123456", password_confirmation:"123456")
 aurelie = User.create(first_name: "Aurelie", last_name:"Test", email: "aurelie@gmail.com", password:"123456", password_confirmation:"123456")
+puts "Users created!"
 
 lorem = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the.
  industry's standard dummy text ever since the 1500s, when an unknown printer took a galley"
@@ -26,3 +34,4 @@ Sneaker.create(brand: "Adidas", name: "Baskets Ozweego", size: "41", price: "100
 Sneaker.create(brand: "Adidas", name: "STAN SMITH - Baskets basses", size: "37", price: "100 €", description: lorem, user_id: eric)
 Sneaker.create(brand: "Nike", name: "Venture runner", size: "38", price: "105 €", description: lorem, user_id: chloé)
 Sneaker.create(brand: "Vans", name: "OLD SKOOL PLATFORM - Baskets basses", size: "35", price: "90 €", description: lorem, user_id: elsa)
+puts "Sneakers created!"
