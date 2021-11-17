f9b4271db6921fd5ce4c7e02bc5564e45c4747ad
Sara Levi <saralevi524@gmail.com>
2021-11-17 14:28:41 +0100

    button-dashboard

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -23,10 +23,16 @@ class BookingsController < ApplicationController
     end
   end
 
+  def update
+    @booking = Booking.find(params[:id])
+    @booking.update(booking_params)
+    raise
+  end
+
   private
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date, :sneaker_id, :user_id)
+    params.require(:booking).permit(:start_date, :end_date, :sneaker_id, :user_id, :approved)
   end
 
   def set_sneaker
